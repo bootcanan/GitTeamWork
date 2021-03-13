0202ddeccad2e381edcf8edffed4b65413cf38d5
Canan Ozdemir <crazyfordata.123@gmail.com>
2021-03-13 22:18:14 +0000

    measaj

--- src/common/CommonArea.java
+++ src/common/CommonArea.java
@@ -4,6 +4,11 @@ public class CommonArea {
 
 	public static void main(String[] args) {
      System.out.println("20:28 - hello World");
+     
+     
+     
+     
+     System.out.println("22:20- Abdullah");
 		
 		
 	}
